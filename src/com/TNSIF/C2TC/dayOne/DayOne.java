1faf879877466c50ca1e95b130f2f57ea5c70369
Yabesh <Yabesh@Lenovo>
2024-04-03 12:23:39 +0530

    First Hello_World commit

---
+++ src/com/TNSIF/C2TC/dayOne/DayOne.java
@@ -0,0 +1,10 @@
+package com.TNSIF.C2TC.dayOne;
+
+public class DayOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Hi");
+      }
+
+}
